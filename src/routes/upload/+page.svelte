4ec7aa4456f2fc91a7821650b05cb8723e21ff8e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-01-06 22:00:02 +0600

    Update +page.svelte

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -1,33 +1,17 @@
 <script lang="ts">
-    import { page } from "$app/stores";
-    import { OpengraphGenerator } from "$functions/opengraph";
+	import Login from './login.svelte';
+	import UploadPage from './upload_page.svelte';
 
-    import Login from "./login.svelte";
-    import UploadPage from "./upload_page.svelte";
+	let token: string | null = null; // check if token is valid
 
-    let token: string | null = null; // check if token is valid
-
-    function on_submit(values: CustomEvent) {
-        token = values.detail;
-    }
-
-    const opengraph_html = new OpengraphGenerator({
-        title: `Upload on AnimeCore`,
-        url: $page.url.href,
-        description: "Upload on animecore",
-        site_name: "CoreProject",
-        locale: "en_US",
-        image_url: ""
-    }).generate_opengraph();
+	function on_submit(values: CustomEvent) {
+		token = values.detail;
+	}
 </script>
 
-<svelte:head>
-    {@html opengraph_html}
-</svelte:head>
-
 {#if token}
-    <UploadPage />
+	<UploadPage />
 {:else}
-    <!-- logic for testing -->
-    <Login on:submit={on_submit} />
+	<!-- logic for testing -->
+	<Login on:submit={on_submit} />
 {/if}
