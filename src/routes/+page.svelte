529f01446dbe609887765e726db128916b882f60
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-01-06 21:11:29 +0600

    add

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-	import AnimeCore from '$icons/logos/anime_core.svelte';
-</script>
+	import { goto } from '$app/navigation';
+	import { beforeUpdate } from 'svelte';
 
-<div class="flex h-screen items-center justify-center gap-3 md:gap-[0.75vw]">
-	<a href="/anime" class="flex items-center gap-3 md:gap-[1vw]">
-		<span class="text-3xl md:text-[2vw] font-semibold text-surface-50">Explore</span>
-		<AnimeCore class="w-40 md:w-[12vw] border-b-[0.15vw] border-surface-300" />
-	</a>
-</div>
+	beforeUpdate(() => {
+		goto('/upload');
+	});
+</script>
