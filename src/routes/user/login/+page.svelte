9c7837c0847bad623d7021a0178fdbc874a503f4
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-01-06 20:56:20 +0600

    add

--- src/routes/user/login/+page.svelte
+++ src/routes/user/login/+page.svelte
@@ -1,17 +1,6 @@
 <script lang="ts">
-	import { page as svelte_store_page } from '$app/stores';
-	import { OpengraphGenerator } from '$functions/opengraph';
 	import LoginFormSkeleton from '$skeletons/login_page.svelte';
 	import type { ComponentType } from 'svelte';
-
-	const opengraph_html = new OpengraphGenerator({
-		title: 'Register Page',
-		site_name: 'CoreProject',
-		image_url: '', // Use Opengraph later
-		url: $svelte_store_page.url.href,
-		locale: 'en_US',
-		description: 'A page where you can register your core account'
-	}).generate_opengraph();
 
 	// We import our page components (similar to the one above).
 	let one = import('./1.svelte');
@@ -43,11 +32,6 @@
 
 	$: current_page = pages[page];
 </script>
-
-<svelte:head>
-	<title>Login | CoreProject</title>
-	{@html opengraph_html}
-</svelte:head>
 
 <!-- We display the current step here -->
 {#await current_page}
