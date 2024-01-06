1abba5b47f3ae05ee8d0ac3ee0678cbe3c6bf6fa
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-01-06 21:27:26 +0600

    add

--- src/lib/store/user.ts
+++ src/lib/store/user.ts
@@ -1,4 +1,4 @@
-import type { Writable } from "svelte/store";
-import { writable } from "svelte/store";
+import type { Writable } from 'svelte/store';
+import { localStorageStore } from '@skeletonlabs/skeleton';
 
-export const is_authenticated: Writable<boolean> = writable(true);
+export const is_authenticated: Writable<boolean> = localStorageStore('authenticated', false);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { beforeUpdate } from 'svelte';
-
+	import { is_authenticated } from '$store/user';
+	import { get } from 'svelte/store';
 	beforeUpdate(() => {
-		goto('/user/login');
+		if (get(is_authenticated) === false) {
+			goto('/user/login');
+		}
 	});
 </script>
