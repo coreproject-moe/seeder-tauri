885574c80c15d759dcf017db95ecf9694141a9e8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-01-07 09:15:43 +0600

    Update login.rs

--- src-tauri/src/login.rs
+++ src-tauri/src/login.rs
@@ -1,4 +1,13 @@
+use reqwest;
+use std::collections::HashMap;
+
 #[tauri::command]
 pub fn login_function(username: String, password: String) -> String {
+    let client = reqwest::Client::new();
+    let mut map = HashMap::new();
+    map.insert("username", username);
+    map.insert("password", password);
+
+    // let res = client.post("http://httpbin.org/post").json(&map).send()?;
     return "I was invoked from JS!".to_string();
 }
