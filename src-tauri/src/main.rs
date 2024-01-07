451f6c58b236646a2f45a05742b640cc0d630dde
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-01-07 09:03:47 +0600

    add

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+pub mod constants;
 pub mod login;
 
 use login::login_function;
