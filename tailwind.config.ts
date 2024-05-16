3c38ae695d293cb4e630da2d80c8136617be3bec
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-05-16 11:48:32 +0600

    Add mock test
    add

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,34 +1,54 @@
-import { join } from "path";
-import type { Config } from "tailwindcss";
-import forms from "@tailwindcss/forms";
-import typography from "@tailwindcss/typography";
-import scrollbar from "tailwind-scrollbar";
-// 1. Import the Skeleton plugin
-import { skeleton } from "@skeletonlabs/tw-plugin";
-import { kokoroTheme } from "./src/lib/themes/kokoro";
+/**
+ * This is a minimal config.
+ *
+ * If you need the full config, get it from here:
+ * https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
+ */
+import type { Config } from 'tailwindcss';
 
-const config = {
-    // 2. Opt for dark mode to be handled via the class method
-    darkMode: "class",
-    content: [
-        "./src/**/*.{html,js,svelte,ts}",
-        // 3. Append the path to the Skeleton package
-        join(require.resolve("@skeletonlabs/skeleton"), "../**/*.{html,js,svelte,ts}")
-    ],
-    theme: {
-        extend: {}
-    },
-    plugins: [
-        // 4. Append the Skeleton plugin (after other plugins)
-        typography,
-        forms,
-        scrollbar,
-        skeleton({
-            themes: {
-                custom: [kokoroTheme]
-            }
-        })
-    ]
+// Import plugins
+import forms from '@tailwindcss/aspect-ratio';
+import typography from '@tailwindcss/typography';
+import aspectRatio from '@tailwindcss/aspect-ratio';
+import scrollbar from 'tailwind-scrollbar';
+import daisyui from 'daisyui';
+
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Kokoro', 'sans-serif']
+			}
+		}
+	},
+	plugins: [
+		/**
+		 * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
+		 * for forms. If you don't like it or have own styling for forms,
+		 * comment the line below to disable '@tailwindcss/forms'.
+		 */
+		forms,
+		typography,
+		aspectRatio,
+		scrollbar,
+		daisyui
+	],
+	daisyui: {
+		themes: [
+			{
+				kokoro: {
+					primary: '#7569E1',
+					secondary: '#03020C',
+					accent: '#FFF7F8',
+					neutral: '#1E2036',
+					'base-100': '#070519',
+					info: '#DCD9F7',
+					success: '#6FCF97',
+					warning: '#EDD68D',
+					error: '#EB5757'
+				}
+			}
+		]
+	}
 } satisfies Config;
-
-export default config;
