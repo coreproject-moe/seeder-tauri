b2de8dcb79a5bcf0ebd9367c86e7e7e7ab81ac59
Moonlit Grace <tokitousan2005@gmail.com>
2024-06-08 01:50:32 +0530

    Refactor: re-init tauri with bare sveltekit (#112)

--- src-tauri/src/login.rs
+++
@@ -1,4 +0,0 @@
-#[tauri::command]
-pub fn login_function(username: String, password: String) -> String {
-    return "I was invoked from JS!".to_string();
-}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-pub mod constants;
-pub mod login;
-
-use login::login_function;
 
 fn main() {
-    tauri::Builder::default()
-        // This is where you pass in your commands
-        .invoke_handler(tauri::generate_handler![login_function])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
